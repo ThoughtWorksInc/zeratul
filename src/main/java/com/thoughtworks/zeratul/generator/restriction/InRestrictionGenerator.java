923949efa54643729233bc02cc12b045cd88fcea
Elvis.Liu <elvisliu.cs@gmail.com>
2015-08-05 10:23:51 +0800

    [Yufeng]: fix in/nin bug

--- src/main/java/com/thoughtworks/zeratul/generator/restriction/InRestrictionGenerator.java
+++ src/main/java/com/thoughtworks/zeratul/generator/restriction/InRestrictionGenerator.java
@@ -2,6 +2,8 @@ package com.thoughtworks.zeratul.generator.restriction;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.StreamSupport;
+
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Predicate;
@@ -13,7 +15,11 @@ public class InRestrictionGenerator extends ComplexRestrictionGeneratorBase<Obje
     }
 
     @Override
-    protected List<Predicate> doGenerate(CriteriaBuilder criteriaBuilder, Root<?> entity, String field, Iterable<Expression<Object>> expressions) {
-        return Arrays.asList(entity.get(field).in(expressions));
+    protected List<Predicate> doGenerate(CriteriaBuilder criteriaBuilder, Root<?> entity, String field,
+                                         Iterable<Expression<Object>> expressions) {
+
+        Expression[] expressionArray = StreamSupport.stream(expressions.spliterator(), false).toArray(
+            Expression[]::new);
+        return Arrays.asList(entity.get(field).in(expressionArray));
     }
 }
